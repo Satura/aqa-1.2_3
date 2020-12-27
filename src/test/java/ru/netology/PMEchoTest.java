d4c137ca363ca0e212bc5c95f21e2451f5d668b4
Anastasia Sv <selen500@mail.ru>
2020-12-27 17:08:06 +0500

    Initial commit

---
+++ src/test/java/ru/netology/PMEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PMEchoTest {
+
+    @Test
+    public void shouldReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some fail data"))
+        ;
+    }
+}
